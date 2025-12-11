130b0cba403b8716cd77d3d02d0b591a78fbed55
AlMikan <nekomimi987@gmail.com>
2025-12-11 17:29:02 +0900

    fix: Update check_vec.py to print file paths instead of array contents

--- batch/check_vec.py
+++ batch/check_vec.py
@@ -1,26 +1,30 @@
 import numpy as np
 
 arr = np.load("./vector/animalkingdom/AAOYRUDX/avg_pooling.npy")
-print(arr)
+print("./vector/animalkingdom/AAOYRUDX/avg_pooling.npy")
 print(arr.shape)
+print("=====================================================")
 
 arr = np.load("./vector/polar/clip_000/avg_pooling.npy")
-print(arr)
+print("./vector/polar/clip_000/avg_pooling.npy")
 print(arr.shape)
+print("=====================================================")
 
 arr = np.load("./x3d_vector/animalkingdom/AAOYRUDX/avg_pooling.npy")
-print(arr)
+print("./x3d_vector/animalkingdom/AAOYRUDX/avg_pooling.npy")
 print(arr.shape)
+print("=====================================================")
 
 arr = np.load("./x3d_vector/polar/clip_000/avg_pooling.npy")
-print(arr)
+print("./x3d_vector/polar/clip_000/avg_pooling.npy")
 print(arr.shape)
-
+print("=====================================================")
 
 arr = np.load("./x3d_vector_centered/animalkingdom/AAOYRUDX/avg_pooling.npy")
-print(arr)
+print("./x3d_vector_centered/animalkingdom/AAOYRUDX/avg_pooling.npy")
 print(arr.shape)
+print("=====================================================")
 
 arr = np.load("./x3d_vector_centered/polar/clip_000/avg_pooling.npy")
-print(arr)
+print("./x3d_vector_centered/polar/clip_000/avg_pooling.npy")
 print(arr.shape)
