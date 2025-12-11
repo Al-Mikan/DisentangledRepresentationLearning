b88bfc378bb01db03cb2cdd18e84d613264c596a
AlMikan <nekomimi987@gmail.com>
2025-12-11 17:27:19 +0900

    feat: Add check_vec.py to load and display average pooling vectors for different datasets

---
+++ batch/check_vec.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr = np.load("./vector/animalkingdom/AAOYRUDX/avg_pooling.npy")
+print(arr)
+print(arr.shape)
+
+arr = np.load("./vector/polar/clip_000/avg_pooling.npy")
+print(arr)
+print(arr.shape)
+
+arr = np.load("./x3d_vector/animalkingdom/AAOYRUDX/avg_pooling.npy")
+print(arr)
+print(arr.shape)
+
+arr = np.load("./x3d_vector/polar/clip_000/avg_pooling.npy")
+print(arr)
+print(arr.shape)
+
+
+arr = np.load("./x3d_vector_centered/animalkingdom/AAOYRUDX/avg_pooling.npy")
+print(arr)
+print(arr.shape)
+
+arr = np.load("./x3d_vector_centered/polar/clip_000/avg_pooling.npy")
+print(arr)
+print(arr.shape)
